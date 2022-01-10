d9c689bddaff7f966e2129a2d794237ba42e6327
Juan Manuel Pérez <jmperez.85@gmail.com>
2022-01-10 10:37:09 -0300

    Updated results

--- scripts/run_lince_benchmarks.sh
+++ scripts/run_lince_benchmarks.sh
@@ -1,22 +1,22 @@
 # These are all dev benchmarks -- LINCE does not provide gold labels for test data
-path="evaluations/lince/ner"
+path="evaluations/lince/ner/dev"
 models=(
-    #"pysentimiento/robertuito-base-uncased"
+    "pysentimiento/robertuito-base-uncased"
     "pysentimiento/robertuito-base-cased"
     "pysentimiento/robertuito-base-deacc"
-    #"xlm-roberta-base"
-    #"bert-base-cased"
-    #"bert-base-uncased"
-    #"dccuchile/bert-base-spanish-wwm-uncased"
-    #"dccuchile/bert-base-spanish-wwm-cased"
-    #"bert-base-multilingual-uncased"
+    "xlm-roberta-base"
+    "bert-base-multilingual-uncased"
+    "bert-base-cased"
+    "bert-base-uncased"
+    "dccuchile/bert-base-spanish-wwm-uncased"
+    "dccuchile/bert-base-spanish-wwm-cased"
 )
 
 for base_model in ${models[@]}
 do
-    output_path="$path/${base_model#*/}.json"
+    output_path="$path/${base_model#*/}_nosublabeling_emojis_last.json"
     python bin/train.py --base_model $base_model \
         --lang es --epochs 5 --task ner \
         --benchmark --benchmark_output_path $output_path \
-        --times 4
+        --times 2
 done
