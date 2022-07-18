74432932f82b1074ebf92496a5b38a622b20acf8
ruanchaves <ruanchaves93@gmail.com>
2022-07-18 22:06:01 +0200

    replace TransformerWordSegmenter by FastWordSegmenter

--- pysentimiento/segmenter.py
+++ pysentimiento/segmenter.py
@@ -1,42 +1,25 @@
-from hashformers.segmenter.auto import TransformerWordSegmenter
+from hashformers.segmenter.auto import FastWordSegmenter
 from hashformers.segmenter import TweetSegmenter, TwitterTextMatcher
 
-models = {
-    "es": "DeepESP/gpt2-spanish",
-    "en": "distilgpt2"
-}
-
-def create_segmenter(lang='en', model_name=None, batch_size=1000):
+def create_segmenter(lang='en'):
     """
     Create a word segmenter.
 
     Arguments:
     ----------
     lang: str
-        Language code (es or en)
-    model_name: str
-        Model name or path
+        Language code.
+        For a full list of the supported languages and their language codes, 
+        check the `wordfreq library documentation <https://pypi.org/project/wordfreq/>`_.
 
     Returns:
     --------
         
     """
-    if lang not in models:
-        raise ValueError(f"Language {lang} not supported -- only supports {models.keys()}")
 
-    if not model_name:
-        segmenter_model_name_or_path = models[lang]
-    else:
-        segmenter_model_name_or_path = model_name
-    
-    word_segmenter = TransformerWordSegmenter(
-        segmenter_model_name_or_path = segmenter_model_name_or_path,
-        segmenter_model_type = "gpt2",
-        segmenter_device = "cuda",
-        segmenter_gpu_batch_size = batch_size,
-        reranker_gpu_batch_size = None,
-        reranker_model_name_or_path = None,
-        reranker_model_type = None
+    word_segmenter = FastWordSegmenter(
+        unigram_lang=lang,
+        reranker_model_name_or_path=None
     )
 
     twitter_matcher = TwitterTextMatcher()
