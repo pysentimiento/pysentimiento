f19c492d0d24875c93e731a43419edd88290c694
Juan Manuel Pérez <jmperez.85@gmail.com>
2023-09-07 15:24:44 -0300

    Use GoEmotions for English

--- pysentimiento/emotion.py
+++ pysentimiento/emotion.py
@@ -13,8 +13,6 @@ Lo pongo así por huggingface
 task_name = "emotion"
 
 
-
-
 def accepts(lang, **kwargs):
     """
     Returns True if the task supports the language
@@ -29,59 +27,64 @@ def load_datasets(lang, preprocess=True, preprocessing_args={}):
 
     if lang in {"es", "en", "it", "pt"}:
         # I add VerificationMode.NO_CHECKS to avoid the error
-        ds = load_dataset(f"pysentimiento/{lang}_emotion", verification_mode=VerificationMode.NO_CHECKS)
+        ds = load_dataset(
+            f"pysentimiento/{lang}_emotion",
+            verification_mode=VerificationMode.NO_CHECKS,
+        )
     else:
         raise ValueError(f"Language {lang} not supported for irony detection")
 
     if preprocess:
+
         def preprocess_fn(ex):
-            return {"text": preprocess_tweet(ex["text"], lang=lang, **preprocessing_args)}
+            return {
+                "text": preprocess_tweet(ex["text"], lang=lang, **preprocessing_args)
+            }
+
         ds = ds.map(preprocess_fn, batched=False)
 
     return ds
 
 
-def train(
-    base_model, lang="es", use_defaults_if_not_tuned=False,
-    **kwargs
-):
-    """
-    """
+def train(base_model, lang="es", use_defaults_if_not_tuned=False, **kwargs):
+    """ """
     ds = load_datasets(
-        lang=lang,
-        preprocessing_args=get_preprocessing_args(base_model, lang=lang)
+        lang=lang, preprocessing_args=get_preprocessing_args(base_model, lang=lang)
     )
-
 
     if "labels" in ds["train"].features:
         emotions = ds["train"].features["labels"].feature.names
         id2label = {i: label for i, label in enumerate(emotions)}
 
         def _convert_labels(ex):
-            labels = [.0] * len(emotions)
+            labels = [0.0] * len(emotions)
             for l in ex["labels"]:
                 labels[l] = 1.0
             return {"labels": labels, "foo": labels}
-
-        feature_names = ds["train"].features["labels"]
 
         ds = ds.map(_convert_labels, batched=False)
         ds = ds.remove_columns("labels")
         ds = ds.rename_column("foo", "labels")
         problem_type = "multi_label_classification"
     else:
-        id2label = {k: v for k, v in enumerate(
-            ds["train"].features["label"].names)}
+        id2label = {k: v for k, v in enumerate(ds["train"].features["label"].names)}
 
     training_args = get_training_arguments(
-        base_model, task_name=task_name, lang=lang,
-        metric_for_best_model="eval/macro_f1", use_defaults_if_not_tuned=use_defaults_if_not_tuned
+        base_model,
+        task_name=task_name,
+        lang=lang,
+        metric_for_best_model="eval/macro_f1",
+        use_defaults_if_not_tuned=use_defaults_if_not_tuned,
     )
 
     return train_and_eval(
-        base_model=base_model, dataset=ds, id2label=id2label,
-        training_args=training_args, lang=lang, problem_type=problem_type,
-        **kwargs
+        base_model=base_model,
+        dataset=ds,
+        id2label=id2label,
+        training_args=training_args,
+        lang=lang,
+        problem_type=problem_type,
+        **kwargs,
     )
 
 
@@ -90,34 +93,30 @@ def hp_tune(base_model, lang, **kwargs):
     Hyperparameter tuning with wandb
     """
     ds = load_datasets(
-        lang=lang,
-        preprocessing_args=get_preprocessing_args(base_model, lang=lang)
+        lang=lang, preprocessing_args=get_preprocessing_args(base_model, lang=lang)
     )
-
 
     if "labels" in ds["train"].features:
         emotions = ds["train"].features["labels"].feature.names
         id2label = {i: label for i, label in enumerate(emotions)}
 
         def _convert_labels(ex):
-            labels = [.0] * len(emotions)
+            labels = [0.0] * len(emotions)
             for l in ex["labels"]:
                 labels[l] = 1.0
             return {"labels": labels, "foo": labels}
-
-        feature_names = ds["train"].features["labels"]
 
         ds = ds.map(_convert_labels, batched=False)
         ds = ds.remove_columns("labels")
         ds = ds.rename_column("foo", "labels")
         problem_type = "multi_label_classification"
     else:
-        id2label = {k: v for k, v in enumerate(
-            ds["train"].features["label"].names)}
-
+        id2label = {k: v for k, v in enumerate(ds["train"].features["label"].names)}
 
     def model_init():
-        model, _ = load_model(base_model, id2label, lang=lang)
+        model, _ = load_model(
+            base_model, id2label, lang=lang, problem_type=problem_type
+        )
         return model
 
     _, tokenizer = load_model(base_model, id2label, lang=lang)
