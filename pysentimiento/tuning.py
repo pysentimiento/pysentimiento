f04ac6a82079178f45359a2edcd4445c19fd4a88
Juan Manuel Pérez <jmperez.85@gmail.com>
2023-11-27 16:52:47 -0300

    Context hate speech hyperparameter tuning

--- pysentimiento/tuning.py
+++ pysentimiento/tuning.py
@@ -4,33 +4,30 @@ from transformers import TrainingArguments, Trainer, DataCollatorWithPadding
 from .config import config
 from .metrics import compute_metrics as _compute_metrics
 
-logger = logging.getLogger('pysentimiento')
+logger = logging.getLogger("pysentimiento")
 logger.setLevel(logging.INFO)
 
 
 # hyperparameters
 parameters_dict = {
-    'epochs': {
-        'values': [3, 4, 5],
+    "epochs": {
+        "values": [3, 4, 5],
     },
-    'batch_size': {
-        'value': 32
-    },
-    'learning_rate': {
-        'values': [2e-5, 3e-5, 5e-5, 6e-5, 7e-5, 8e-5, 1e-4]
-    },
-
-    'weight_decay': {
-        'value': 0.1
-    },
-
-    'warmup_ratio': {
-        'values': [0.06, 0.08, 0.10]
-    },
+    "batch_size": {"value": 32},
+    "learning_rate": {"values": [2e-5, 3e-5, 5e-5, 6e-5, 7e-5, 8e-5, 1e-4]},
+    "weight_decay": {"value": 0.1},
+    "warmup_ratio": {"values": [0.06, 0.08, 0.10]},
 }
 
 
-def get_training_arguments(model_name, task_name, lang, metric_for_best_model, use_defaults_if_not_tuned=False, dont_report=False):
+def get_training_arguments(
+    model_name,
+    task_name,
+    lang,
+    metric_for_best_model,
+    use_defaults_if_not_tuned=False,
+    dont_report=False,
+):
     """
     Get training arguments for a given model and task
 
@@ -50,7 +47,9 @@ def get_training_arguments(model_name, task_name, lang, metric_for_best_model, u
         sweeps = api.project("pysentimiento").sweeps()
         # Get project sweep
         sweep = next(
-            sweep for sweep in sweeps if sweep.name == f"swp-{task_name}-{lang}-{model_name}"
+            sweep
+            for sweep in sweeps
+            if sweep.name == f"swp-{task_name}-{lang}-{model_name}"
         )
 
         # Get best run
@@ -60,15 +59,11 @@ def get_training_arguments(model_name, task_name, lang, metric_for_best_model, u
         tuned_params = best_run.config
 
         # Log it
-        logger.info(
-            f"Model {model_name} tuned for task {task_name} in language {lang}")
+        logger.info(f"Model {model_name} tuned for task {task_name} in language {lang}")
 
         # Just log important params
-        logger.info(
-            {k: v for k, v in tuned_params.items() if k in parameters_dict}
-        )
+        logger.info({k: v for k, v in tuned_params.items() if k in parameters_dict})
     except StopIteration:
-
         if use_defaults_if_not_tuned:
             tuned_params = {}
         else:
@@ -76,7 +71,7 @@ def get_training_arguments(model_name, task_name, lang, metric_for_best_model, u
             raise ValueError(error_msg)
 
     args = TrainingArguments(
-        output_dir=f'./results/{task_name}-{lang}-{model_name}',
+        output_dir=f"./results/{task_name}-{lang}-{model_name}",
         num_train_epochs=tuned_params.get("epochs", 3),
         per_device_train_batch_size=tuned_params.get("batch_size", 32),
         per_device_eval_batch_size=tuned_params.get("batch_size", 32),
@@ -88,7 +83,7 @@ def get_training_arguments(model_name, task_name, lang, metric_for_best_model, u
         do_eval=False,
         weight_decay=tuned_params.get("weight_decay", 0.01),
         fp16=True,
-        logging_dir='./logs',
+        logging_dir="./logs",
         load_best_model_at_end=True,
         metric_for_best_model="macro_f1",
         group_by_length=True,
@@ -102,8 +97,20 @@ def get_training_arguments(model_name, task_name, lang, metric_for_best_model, u
 
 
 def hyperparameter_sweep(
-        name, group_name, model_init, tokenizer, datasets, id2label, sweep_method="random", format_dataset=None, compute_metrics=None, config_info=None, tokenize_fun=None,
-        metric_for_best_model="eval_macro_f1", count=None):
+    name,
+    group_name,
+    model_init,
+    tokenizer,
+    datasets,
+    id2label,
+    sweep_method="random",
+    format_dataset=None,
+    compute_metrics=None,
+    config_info=None,
+    tokenize_fun=None,
+    metric_for_best_model="eval_macro_f1",
+    count=None,
+):
     """
     Hyperparameter sweep with wandb
 
@@ -120,33 +127,32 @@ def hyperparameter_sweep(
         config_info (dict, optional): config info. Defaults to None.
     """
     sweep_config = {
-        'name': name,
-        'method': sweep_method,
-        'parameters': parameters_dict,
+        "name": name,
+        "method": sweep_method,
+        "parameters": parameters_dict,
     }
 
     if compute_metrics is None:
-        def compute_metrics(preds): return _compute_metrics(preds, id2label)
+
+        def compute_metrics(preds):
+            return _compute_metrics(preds, id2label)
 
     def _tokenize_fun(x):
         if tokenize_fun:
             return tokenize_fun(x)
         else:
-            return tokenizer(x['text'], padding=False, truncation=True)
+            return tokenizer(x["text"], padding=False, truncation=True)
 
-    tokenized_ds = datasets.map(
-        _tokenize_fun, batched=True
-    )
+    tokenized_ds = datasets.map(_tokenize_fun, batched=True)
 
     if format_dataset is not None:
         tokenized_ds = tokenized_ds.map(format_dataset)
 
-    tokenized_ds = tokenized_ds.remove_columns([
-        x for x in datasets['train'].column_names if x not in ['labels', 'label']
-    ])
+    tokenized_ds = tokenized_ds.remove_columns(
+        [x for x in datasets["train"].column_names if x not in ["labels", "label"]]
+    )
 
     def train(config=None):
-
         init_params = {
             "config": config or {},
             "group": group_name,
@@ -162,21 +168,22 @@ def hyperparameter_sweep(
 
             # set training arguments
             training_args = TrainingArguments(
-                output_dir='./tmp/sweeps',
-                report_to='wandb',  # Turn on Weights & Biases logging
+                output_dir="./tmp/sweeps",
+                report_to="wandb",  # Turn on Weights & Biases logging
                 num_train_epochs=config.epochs,
                 learning_rate=config.learning_rate,
                 weight_decay=config.weight_decay,
                 per_device_train_batch_size=config.batch_size,
                 warmup_ratio=config.warmup_ratio,
                 per_device_eval_batch_size=config.batch_size,
-                evaluation_strategy='epoch',
-                save_strategy='epoch',
-                logging_strategy='epoch',
+                evaluation_strategy="epoch",
+                save_strategy="epoch",
+                logging_strategy="epoch",
                 load_best_model_at_end=True,
                 metric_for_best_model=metric_for_best_model,
                 remove_unused_columns=False,
                 group_by_length=True,
+                fp16=True,
             )
 
             # define training loop
@@ -185,11 +192,10 @@ def hyperparameter_sweep(
                 model_init=model_init,
                 args=training_args,
                 compute_metrics=compute_metrics,
-                train_dataset=tokenized_ds['train'],
-                eval_dataset=tokenized_ds['dev'],
+                train_dataset=tokenized_ds["train"],
+                eval_dataset=tokenized_ds["dev"],
                 tokenizer=tokenizer,
-                data_collator=DataCollatorWithPadding(
-                    tokenizer, padding="longest"),
+                data_collator=DataCollatorWithPadding(tokenizer, padding="longest"),
             )
 
             # start training loop
--- scripts/hp_tune.sh
+++ scripts/hp_tune.sh
@@ -1,20 +1,34 @@
 #/bin/bash
-models=(
-    "pysentimiento/robertuito-base-uncased"
-    "dccuchile/bert-base-spanish-wwm-cased"
-    "PlanTL-GOB-ES/roberta-base-bne"
-    #"bertin-project/bertin-roberta-base-spanish"
-    #"mrm8488/electricidad-base-discriminator"
-)
 
-tasks=(
-    #"sentiment"
-    #"emotion"
-    #"irony"
-    #"hate_speech"
-    #targeted_sa
-    "context_hate_speech"
-)
+if [ -z "$1" ]
+then
+    tasks=(
+        "irony"
+        "sentiment"
+        "emotion"
+        "hate_speech"
+        "targeted_sa"
+        "context_hate_speech"
+    )
+else
+    tasks=("$1")
+fi
+
+
+# if model is empty, run all models
+
+if [ -z "$2" ]
+then
+    models=(
+        "pysentimiento/robertuito-base-uncased"
+        "dccuchile/bert-base-spanish-wwm-cased"
+        "PlanTL-GOB-ES/roberta-base-bne"
+        "bertin-project/bertin-roberta-base-spanish"
+        "mrm8488/electricidad-base-discriminator"
+    )
+else
+    models=("$2")
+fi
 
 for model in "${models[@]}"
 do
